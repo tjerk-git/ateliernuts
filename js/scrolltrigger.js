e1ee2b43ae54ba81a4cb5c2e11df773ea981c02f
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-01-03 14:13:08 +0100

    first commit

---
+++ js/scrolltrigger.js
@@ -0,0 +1,33 @@
+// GSAP Tween using MotionPath Plugin
+var tween = gsap.to(".bug", {
+  duration: 20,
+  ease: "sine.out",
+  motionPath: {
+    curviness: 1.75,
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5],
+    path: [
+      { x: 100, y: -20 },
+      { x: 350, y: 20 },
+      { x: 500, y: -20 },
+      { x: 400, y: 50 },
+      { x: 500, y: -50 },
+      { x: 700, y: -10 },
+      { x: 900, y: 50 },
+      { x: window.innerWidth, y: 100 }
+    ]
+  }
+});
+
+// ScrollMagic - Controlling the animation from scroll
+var controller = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({
+  duration: 8000, // or window.innerHeight
+  triggerElement: ".trigger-element",
+  triggerHook: 0
+})
+  .setTween(tween)
+  .addIndicators()
+  .setPin(".trigger-element")
+  .addTo(controller);
+
