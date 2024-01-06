44efa3dfd2893a9a836e581ebaa4c711a3751856
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-01-06 10:39:35 +0100

    don't do this on mobile

--- js/cursor.js
+++ js/cursor.js
@@ -1,67 +1,98 @@
+const isUserUsingMobile = () => {
+
+  // User agent string method
+  let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  // Screen resolution method
+  if (!isMobile) {
+    let screenWidth = window.screen.width;
+    let screenHeight = window.screen.height;
+    isMobile = (screenWidth < 768 || screenHeight < 768);
+  }
+
+  // Touch events method
+  if (!isMobile) {
+    isMobile = (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+  }
+
+  // CSS media queries method
+  if (!isMobile) {
+    let bodyElement = document.getElementsByTagName('body')[0];
+    isMobile = window.getComputedStyle(bodyElement).getPropertyValue('content').indexOf('mobile') !== -1;
+  }
+
+  return isMobile
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
-  var cursor = document.querySelector(".cursor");
 
-  function moveCursor(e) {
-    cursor.style.top = e.clientY - cursor.offsetHeight / 2 + "px";
-    cursor.style.left = e.clientX - cursor.offsetWidth / 2 + "px";
-  }
+  if (!isUserUsingMobile()) {
 
-  window.addEventListener("mousemove", moveCursor);
+    var cursor = document.querySelector(".cursor");
 
-  window.addEventListener("touchmove", function (e) {
-    // Touch events have different properties, so we need to adjust the code
-    var touch = e.touches[0];
-    moveCursor({
-      clientX: touch.clientX,
-      clientY: touch.clientY
-    });
-  });
+    function moveCursor(e) {
+      cursor.style.top = e.clientY - cursor.offsetHeight / 2 + "px";
+      cursor.style.left = e.clientX - cursor.offsetWidth / 2 + "px";
+    }
 
-  function hideCursor() {
-    cursor.style.opacity = "0";
-  }
+    window.addEventListener("mousemove", moveCursor);
 
-  function showCursor() {
-    cursor.style.opacity = "1";
-  }
-
-  window.addEventListener("mouseleave", hideCursor);
-  window.addEventListener("touchcancel", hideCursor);
-
-  window.addEventListener("mouseenter", showCursor);
-  window.addEventListener("touchstart", showCursor);
-
-  document.querySelectorAll(".link").forEach(function (link) {
-    link.addEventListener("mouseenter", function () {
-      cursor.style.transform = "scale(3.2)";
+    window.addEventListener("touchmove", function (e) {
+      // Touch events have different properties, so we need to adjust the code
+      var touch = e.touches[0];
+      moveCursor({
+        clientX: touch.clientX,
+        clientY: touch.clientY
+      });
     });
 
-    link.addEventListener("touchstart", function () {
-      cursor.style.transform = "scale(3.2)";
+    function hideCursor() {
+      cursor.style.opacity = "0";
+    }
+
+    function showCursor() {
+      cursor.style.opacity = "1";
+    }
+
+    window.addEventListener("mouseleave", hideCursor);
+    window.addEventListener("touchcancel", hideCursor);
+
+    window.addEventListener("mouseenter", showCursor);
+    window.addEventListener("touchstart", showCursor);
+
+    document.querySelectorAll("a").forEach(function (link) {
+      link.addEventListener("mouseenter", function () {
+        cursor.style.transform = "scale(3.2)";
+      });
+
+      link.addEventListener("touchstart", function () {
+        cursor.style.transform = "scale(3.2)";
+      });
+
+      link.addEventListener("mouseleave", function () {
+        cursor.style.transform = "scale(1)";
+      });
+
+      link.addEventListener("touchend", function () {
+        cursor.style.transform = "scale(1)";
+      });
     });
 
-    link.addEventListener("mouseleave", function () {
+    window.addEventListener("mousedown", function () {
+      cursor.style.transform = "scale(0.2)";
+    });
+
+    window.addEventListener("touchstart", function () {
+      cursor.style.transform = "scale(0.2)";
+    });
+
+    window.addEventListener("mouseup", function () {
       cursor.style.transform = "scale(1)";
     });
 
-    link.addEventListener("touchend", function () {
+    window.addEventListener("touchend", function () {
       cursor.style.transform = "scale(1)";
     });
-  });
-
-  window.addEventListener("mousedown", function () {
-    cursor.style.transform = "scale(0.2)";
-  });
-
-  window.addEventListener("touchstart", function () {
-    cursor.style.transform = "scale(0.2)";
-  });
-
-  window.addEventListener("mouseup", function () {
-    cursor.style.transform = "scale(1)";
-  });
-
-  window.addEventListener("touchend", function () {
-    cursor.style.transform = "scale(1)";
-  });
+  }
 });
